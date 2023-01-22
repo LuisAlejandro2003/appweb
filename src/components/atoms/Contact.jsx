1d9a0b59829ca6055c9893c7b5f2671d82d83cd0
LuisAlejandro2003 <luisalejandro2003121822@gmail.com>
2023-01-21 23:29:42 -0600

    tercer commit

--- src/components/atoms/Contact.jsx
+++ src/components/atoms/Contact.jsx
@@ -1,12 +1,37 @@
-import styles from "../../assets/styles/Contact.css"
+import styles from "../../assets/styles/Contact.css";
 import Title from "./Title";
+import Email from "../../assets/images/Email.svg";
+import Text from "./Text";
+import Phone from "../../assets/images/Phone.svg"
+import Ubication from "../../assets/images/Vector.svg";
 const Contact = () => {
-    return ( 
-        <div className="Contact">
-          <Title>Contacto</Title>
-          <p>+52 961 3715046</p>
+  return (
+    <div className="Contact">
+      <Title>Contacto</Title>
+      <div className="Email">
+        <div className="imagen">
+        <img src={Email} alt="Email" />
         </div>
-     );
-}
- 
+        <Text>luisalejandro2003@gmail.com</Text>
+      </div>
+
+      <div className="Phone">
+        <div className="imagen">
+        <img src={Phone} alt="Phone" />
+        </div>
+        <Text>+52 961 3715046</Text>
+      </div>
+
+      <div className="Ubication">
+        <div className="imagen">
+        <img src={Ubication} alt="Phone" />
+        </div>
+        <Text>TGZ/Chiapas</Text>
+      </div>
+
+
+    </div>
+  );
+};
+
 export default Contact;
